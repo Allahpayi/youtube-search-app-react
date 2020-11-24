6a2d7e9576dfaecafccb425832dcceb297cd4b17
Allahpayi <58375561+Allahpayi@users.noreply.github.com>
2020-11-24 19:29:36 +0400

    Update VideoList.js

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,13 +2,19 @@ import React, { Component } from 'react'
 import Video from "./Video";
 
 export default class VideoList extends Component {
+   
   render() {
+
+    console.log(this.props.data)
+
     return (
       <div className="row">
         <div className="col-md-12">
           <div className="row">
             <div className="col-md-12">
-              <h3 className="video-list-title">Other Videos</h3>
+              {
+                this.props.data.length === 0 ? "" : <h3 className="video-list-title">Other Videos</h3>
+              }
             </div>
           </div>
           <div className="row" >
